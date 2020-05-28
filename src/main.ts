9cffe965939728e67e9fc127b53f290a095b1ee5
chendaliang <piaoyaocdl@163.com>
2020-05-28 15:28:11 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap()
+{
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+
+bootstrap()
+  .then(value => console.log('系统启动'))
+  .catch(reason => console.log(('系统异常')))
