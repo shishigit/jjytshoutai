5501118117ff8e433d08b14e3f28ac87964ac04b
chendaliang <piaoyaocdl@163.com>
2020-05-28 17:34:27 +0800

    1
    Signed-off-by: chendaliang <piaoyaocdl@163.com>

--- src/config/rizhi.ts
+++ src/config/rizhi.ts
@@ -1,8 +1,42 @@
 /******************************************************************************
  * 系统运行日志
  ******************************************************************************/
-import { configure, getLogger } from 'log4js';
+import { configure, getLogger, Logger } from 'log4js';
 import { peizhiwenjian } from './peizhiwenjian';
+import { LoggerService } from '@nestjs/common';
+
+class XitongRizhi implements LoggerService
+{
+  constructor(private readonly logger: Logger)
+  {
+  }
+
+  debug(message: any, context?: string): any
+  {
+    this.logger.debug(context ? `${context}: ${message}` : message);
+  }
+
+  error(message: any, trace?: string, context?: string): any
+  {
+    this.logger.error(context ? `${context}: ${message}` : message);
+    this.logger.error(trace);
+  }
+
+  log(message: any, context?: string): any
+  {
+    this.logger.info(context ? `${context}: ${message}` : message);
+  }
+
+  verbose(message: any, context?: string): any
+  {
+    this.logger.trace(context ? `${context}: ${message}` : message);
+  }
+
+  warn(message: any, context?: string): any
+  {
+    this.logger.warn(context ? `${context}: ${message}` : message);
+  }
+}
 
 configure({
   appenders: {
@@ -15,4 +49,4 @@ configure({
   },
 });
 
-export const rizhi = getLogger(peizhiwenjian.kaifa ? 'default' : 'shengchan');
+export const rizhi = new XitongRizhi(getLogger(peizhiwenjian.kaifa ? 'default' : 'shengchan'));
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { rizhi } from './config/rizhi';
+import { peizhiwenjian } from './config/peizhiwenjian';
 
 async function bootstrap()
 {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { logger: rizhi });
+  await app.listen(peizhiwenjian.duankou);
 }
 
 bootstrap()
-  .then(value => rizhi.info('系统启动'))
-  .catch(reason => rizhi.info(('系统异常')));
+  .then(value => rizhi.log('系统启动'))
+  .catch(reason => rizhi.log(('系统异常')));
