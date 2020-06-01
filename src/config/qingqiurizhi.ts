e743821193c414213a84df3ee4ff79ba58e093ef
chendaliang <piaoyaocdl@163.com>
2020-06-01 10:07:31 +0800

    1

--- src/config/qingqiurizhi.ts
+++ src/config/qingqiurizhi.ts
@@ -2,11 +2,49 @@
  * 开发时候，打印请求
  ******************************************************************************/
 import { rizhi } from './rizhi';
-import { Request } from 'express';
+import { Request, Response } from 'express';
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 export function qingqiurizhi(req: Request, res, next)
 {
   rizhi.verbose(`请求URL： ${req.originalUrl}`);
   rizhi.verbose(`请求BODY： ${req.body}`);
   next();
+}
+
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor
+{
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any>
+  {
+    if (context.getType() === 'http')
+    {
+      let req = context.switchToHttp().getRequest<Request>();
+      let res = context.switchToHttp().getResponse<Response>();
+
+      const now = Date.now();
+
+      return next
+        .handle()
+        .pipe(
+          tap(() =>
+          {
+            rizhi.verbose(`请求
+URL： ${req.originalUrl}
+METHOD: ${req.method}
+BODY：
+${JSON.stringify(req.body, null, 4)}
+返回： ${res.statusCode}
+用时：${Date.now() - now}ms
+            `);
+          }),
+        );
+    }
+
+    rizhi.verbose(`未处理的日志： ${context.getType()}`);
+    return next.handle();
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { rizhi } from './config/rizhi';
 import { peizhiwenjian } from './config/peizhiwenjian';
-import { qingqiurizhi } from './config/qingqiurizhi';
+import { LoggingInterceptor } from './config/qingqiurizhi';
 import { HttpExceptionFilter } from './config/yichang';
 import { Shujukubanben } from './config/shujukubanben';
 
@@ -10,7 +10,7 @@ async function bootstrap()
 {
   const app = await NestFactory.create(AppModule, { logger: rizhi });
 
-  if (peizhiwenjian.kaifa) app.use(qingqiurizhi);
+  if (peizhiwenjian.kaifa) app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
 
   await Shujukubanben.tongbushuju();
