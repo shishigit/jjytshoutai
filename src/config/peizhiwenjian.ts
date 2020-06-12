0cc36c98b5d35fd2c5e0b84758d6d5958d09a8c3
chendaliang <piaoyaocdl@163.com>
2020-06-12 15:08:49 +0800

    1

--- src/config/peizhiwenjian.ts
+++ src/config/peizhiwenjian.ts
@@ -1,73 +1,73 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
-import { Yonghu } from '../db/yonghu';
-import { Banben } from '../db/banben';
-import { Jiekou } from '../db/jiekou';
-import { Juese } from '../db/juese';
+import {TypeOrmModuleOptions} from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
+import {Yonghu} from '../db/yonghu';
+import {Banben} from '../db/banben';
+import {Jiekou} from '../db/jiekou';
+import {Juese} from '../db/juese';
 
 /**
  * 配置文件：整个系统的配置，用于配置开发环境和生产环境
  */
 interface Peizhiwenjian
 {
-  // 开发环境？生产环境？
+    // 开发环境？生产环境？
 
-  kaifa: boolean,
+    kaifa: boolean,
 
-  // 监听端口
-  duankou: number
+    // 监听端口
+    duankou: number
 
-  // 数据库
-  shujuku: TypeOrmModuleOptions
+    // 数据库
+    shujuku: TypeOrmModuleOptions
 
-  // Session
-  session: {
-    maxAge: number;
-  };
+    // Session
+    session: {
+        maxAge: number;
+    };
 
-  // redis
-  redis: {
-    port: number;
-    host: string;
-  };
+    // redis
+    redis: {
+        port: number;
+        host: string;
+    };
 }
 
 const entities = [Yonghu, Banben, Jiekou, Juese];
 
 // 开发环境
 const kaifahuanjing: Peizhiwenjian = {
-  redis: { host: '127.0.0.1', port: 6379 },
-  session: { maxAge: 30 * 60 * 1000 },
-  duankou: 3100,
-  kaifa: true,
-  shujuku: {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'shishi',
-    database: 'jjytshoutai',
-    entities,
-    synchronize: true,
-  },
+    redis: {host: '127.0.0.1', port: 6379},
+    session: {maxAge: 30 * 60 * 1000},
+    duankou: 3100,
+    kaifa: true,
+    shujuku: {
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'shishi',
+        database: 'jjytshoutai',
+        entities,
+        synchronize: true,
+    },
 };
 
 // 生产环境
 // noinspection JSUnusedLocalSymbols
 const shengchanhuanjing: Peizhiwenjian = {
-  redis: { host: '127.0.0.1', port: 6379 },
-  session: { maxAge: 30 * 60 * 1000 },
-  duankou: 3100,
-  kaifa: false,
-  shujuku: {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'shishi',
-    database: 'jjytshoutai',
-    entities,
-    synchronize: true,
-  },
+    redis: {host: '127.0.0.1', port: 6379},
+    session: {maxAge: 30 * 60 * 1000},
+    duankou: 3100,
+    kaifa: false,
+    shujuku: {
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'shishi',
+        database: 'jjytshoutai',
+        entities,
+        synchronize: true,
+    },
 };
 
 export const peizhiwenjian: Peizhiwenjian = kaifahuanjing;
