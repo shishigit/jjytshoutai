2f28d9caff8b8e32c1b705928ded4d11372cf52f
chendaliang <piaoyaocdl@163.com>
2020-07-11 22:02:50 +0800

    1

--- src/ctrl/ctrl.xitong.ts
+++ src/ctrl/ctrl.xitong.ts
@@ -7,33 +7,49 @@ import {Yonghu} from '../db/yonghu';
 import {JiekouSql} from '../db/jiekou.sql';
 import {JueseSql} from '../db/juese.sql';
 import {jiami} from "../config/gongju";
+import {xitong} from "./ctrl.jiekou";
+import denglu = xitong.denglu;
 
 @JJYController('xitong', '系统接口')
 export class CtrlXitong
 {
-  @JJYPost('denglu', '系统登陆', 'niming')
-  async denglu(
-      @JJYBody('zhanghao') zhanghao: string,
-      @JJYBody('mima') mima: string,
-      @Session() session: JJYSession,
-  )
-  {
-    if (!zhanghao) throw  new YichangTishi('账号不能为空');
-    if (!mima) throw  new YichangTishi('密码不能为空');
-    let yonghu: Yonghu = await YonghuSql.findByZhanghao(zhanghao);
-    if (!yonghu || !yonghu.jihuo) throw new YichangTishi('账号或者密码错误！');
-    let fuhe = jiami.fuhe(mima, yonghu.mima);
-    if (!fuhe) throw new YichangTishi('账号或者密码错误！');
+    @JJYPost('denglu', '系统登陆', 'niming')
+    async denglu(
+        @JJYBody('zhanghao') zhanghao: string,
+        @JJYBody('mima') mima: string,
+        @Session() session: JJYSession,
+    ): Promise<denglu[]>
+    {
+        if (!zhanghao) throw  new YichangTishi('账号不能为空');
+        if (!mima) throw  new YichangTishi('密码不能为空');
+        let yonghu: Yonghu = await YonghuSql.findByZhanghao(zhanghao);
+        if (!yonghu || !yonghu.jihuo) throw new YichangTishi('账号或者密码错误！');
+        let fuhe = jiami.fuhe(mima, yonghu.mima);
+        if (!fuhe) throw new YichangTishi('账号或者密码错误！');
 
-    session.yonghu = {id: yonghu.id, zhanghao: yonghu.zhanghao};
+        session.yonghu = {id: yonghu.id, zhanghao: yonghu.zhanghao};
 
-    let juesesid = (await JueseSql.findByYonghuId(yonghu.id)).map(value => value.id);
-    let jiekous = await JiekouSql.findByJueseids(juesesid);
+        let juesesid = (await JueseSql.findByYonghuId(yonghu.id)).map(value => value.id);
+        let jiekous = await JiekouSql.findByJueseids(juesesid);
 
-    session.jiekous = jiekous
-      .filter(value => value.jianquan === 'jianquan')
-      .map(value => value.url);
+        session.jiekous = jiekous
+            .filter(value => value.jianquan === 'jianquan')
+            .map(value => value.url);
 
-    return jiekous.filter(value => value.jianquan === 'jianquan');
-  }
+        let ret: denglu[] = []
+
+        jiekous
+            .filter(value => value.jianquan === 'jianquan')
+            .forEach(value => ret.push(
+                {
+                    fenzu: value.fenzu,
+                    id: value.id,
+                    method: value.method,
+                    qiyong: value.qiyong,
+                    shuoming: value.shuoming,
+                    url: value.url
+                }))
+
+        return ret;
+    }
 }
--- src/ctrl/ctrl.yonghuguanli.ts
+++ src/ctrl/ctrl.yonghuguanli.ts
@@ -3,14 +3,52 @@ import {YichangTishi} from '../config/yichang';
 import {YonghuSql} from '../db/yonghu.sql';
 import {Yonghu} from '../db/yonghu';
 import {jiami, stringUtil} from "../config/gongju";
+import {yonghu} from "./ctrl.jiekou";
+import tianjia = yonghu.tianjia;
+import chaxun = yonghu.chaxun;
+import jihuo = yonghu.jihuo;
+
 
 @JJYController('yonghu', '用户管理接口')
 export class CtrlYonghuguanli
 {
+    @JJYPost('chaxun', '查询用户')
+    async chaxun(): Promise<chaxun>
+    {
+        let ls = await YonghuSql.findAndCount();
+
+        return {
+            yonghu: ls[0].map(value =>
+            {
+                return {
+                    id: value.id,
+                    jihuo: value.jihuo,
+                    zhanghao: value.zhanghao
+                }
+            }),
+            zongshu: ls[1]
+        }
+    }
+
+    @JJYPost('jihuo', '激活用户')
+    async jihuo(
+        @JJYBody('id') id: number,
+        @JJYBody('jihuo') jihuo: boolean,
+        @JJYBody('jihuo1') jihuo1: boolean,
+    ): Promise<jihuo>
+    {
+        if (!id) throw new YichangTishi('没有选取操作的用户！');
+        if (jihuo === undefined) throw new YichangTishi('没有指明是否激活！');
+        let yonghu = await YonghuSql.findById(id);
+        if (!yonghu) throw new YichangTishi('没有找到该用户！')
+        yonghu.jihuo = jihuo;
+        await yonghu.save()
+    }
+
     @JJYPost('tianjia', '添加用户')
     async tianjia(
         @JJYBody('zhanghao') zhanghao: string,
-    )
+    ): Promise<tianjia>
     {
         if (!zhanghao || !stringUtil.isZhimuShuzi(zhanghao))
             throw new YichangTishi('账号应为字母、数字组合！');
@@ -23,28 +61,5 @@ export class CtrlYonghuguanli
         yonghu.jihuo = true;
         yonghu.mima = jiami.jiami('123456');
         await yonghu.save();
-    }
-
-    @JJYPost('chaxun', '查询用户')
-    async chaxun()
-    {
-        let ret = await YonghuSql.findAndCount();
-        ret[0].forEach(value => delete value.mima)
-        return ret
-    }
-
-    @JJYPost('jihuo', '激活用户')
-    async jihuo(
-        @JJYBody('id') id: number,
-        @JJYBody('jihuo') jihuo: boolean,
-        @JJYBody('jihuo1') jihuo1: boolean,
-    )
-    {
-        if (!id) throw new YichangTishi('没有选取操作的用户！');
-        if (jihuo === undefined) throw new YichangTishi('没有指明是否激活！');
-        let yonghu = await YonghuSql.findById(id);
-        if (!yonghu) throw new YichangTishi('没有找到该用户！')
-        yonghu.jihuo = jihuo;
-        await yonghu.save()
     }
 }
