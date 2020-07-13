1cb076e443572d7a57a114c72012bf8d2ef67822
chendaliang <piaoyaocdl@163.com>
2020-07-13 16:28:41 +0800

    1

--- src/db/entities/bumen.ts
+++ src/db/entities/bumen.ts
@@ -1,7 +1,18 @@
-import {BaseEntity, Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn} from "typeorm";
+import {
+    BaseEntity,
+    Column,
+    Entity,
+    JoinTable,
+    ManyToMany,
+    PrimaryGeneratedColumn,
+    Tree,
+    TreeChildren,
+    TreeParent
+} from "typeorm";
 import {Yonghu} from "./yonghu";
 
 @Entity()
+@Tree("materialized-path")
 export class Bumen extends BaseEntity
 {
     @PrimaryGeneratedColumn()
@@ -16,4 +27,10 @@ export class Bumen extends BaseEntity
     @ManyToMany(() => Yonghu, yonghu => yonghu.bumens)
     @JoinTable({name: 'bumen_yonghu'})
     yonghus: Yonghu[];
+
+    @TreeChildren()
+    children: Bumen[];
+
+    @TreeParent()
+    parent: Bumen;
 }
