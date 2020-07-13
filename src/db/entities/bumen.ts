eea98b9cff75abea1a4ecdc8384d1c33be62f886
chendaliang <piaoyaocdl@163.com>
2020-07-13 14:42:18 +0800

    1

--- src/db/entities/bumen.ts
+++ src/db/entities/bumen.ts
@@ -4,12 +4,15 @@ import {Yonghu} from "./yonghu";
 @Entity()
 export class Bumen extends BaseEntity
 {
-    @Column({width: 200})
-    beizhu: string
     @PrimaryGeneratedColumn()
     id: number;
+
+    @Column({width: 200})
+    beizhu: string
+
     @Column({width: 50, unique: true, nullable: false})
     mingcheng: string
+
     @ManyToMany(() => Yonghu, yonghu => yonghu.bumens)
     @JoinTable({name: 'bumen_yonghu'})
     yonghus: Yonghu[];
